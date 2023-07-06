ab5917af115c29ce65e7631918965bd86f40ebc0
nehanarta <nehabaswanthrao@gmail.com>
2023-07-06 18:26:13 +0530

    Added:validation lastName

--- src/com/bridglabz/UserValidation.java
+++ src/com/bridglabz/UserValidation.java
@@ -15,11 +15,25 @@ public class UserValidation {
             System.out.println("is invalid");
         }
     }
+    void lastName(){
+        Pattern pattern=Pattern.compile("[A-Za-z]{3,}");
+        System.out.println("Enter the lastName");
+        Scanner sc=new Scanner(System.in);
+        String lastName=sc.next();
+        Matcher matcher=pattern.matcher(lastName);
+        if(matcher.matches()){
+            System.out.println("is valid");
+        }else{
+            System.out.println("is invalid");
+        }
+    }
+
 
 
     public static void main(String[] args) {
         UserValidation e=new UserValidation();
         e.firstName();
+        e.lastName();
 
 
     }

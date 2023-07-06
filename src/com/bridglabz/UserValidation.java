2be43089e0c2886be78e98dc2bd1b35afc182a81
nehanarta <nehabaswanthrao@gmail.com>
2023-07-06 18:24:37 +0530

    Added:validation firstName

--- src/com/bridglabz/UserValidation.java
+++ src/com/bridglabz/UserValidation.java
@@ -3,10 +3,23 @@ import java.util.*;
 import java.util.regex.*;
 
 public class UserValidation {
+    void firstName(){
+        Pattern pattern=Pattern.compile("[A-Za-z]{3,}");
+        System.out.println("Enter the firstName");
+        Scanner sc=new Scanner(System.in);
+        String firstName=sc.next();
+        Matcher matcher=pattern.matcher(firstName);
+        if(matcher.matches()){
+            System.out.println("is valid");
+        }else{
+            System.out.println("is invalid");
+        }
+    }
+
 
     public static void main(String[] args) {
         UserValidation e=new UserValidation();
-        System.out.println("welcome to the regex");
+        e.firstName();
 
 
     }

6dede94dfec2c5358e135b958e4c0c190e41d000
nehanarta <nehabaswanthrao@gmail.com>
2023-07-06 17:59:49 +0530

    Added:welcome to the regex problem

---
+++ src/com/bridglabz/UserValidation.java
@@ -0,0 +1,13 @@
+package com.bridglabz;
+import java.util.*;
+import java.util.regex.*;
+
+public class UserValidation {
+
+    public static void main(String[] args) {
+        UserValidation e=new UserValidation();
+        System.out.println("welcome to the regex");
+
+
+    }
+}
